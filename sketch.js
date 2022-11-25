2d365c56daea6df430a3346ee361173a7d8d0f3b
gauthier909 <78608563+grogerfr@users.noreply.github.com>
2022-11-25 13:11:34 +0100

    delete useless files

--- sketch.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-  createCanvas(1000, 1000);
-}
-
-function draw() {
-  background(220);
-}
--- sketch2.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    createCanvas(1000, 1000);
-  }
-  
-  function draw() {
-    background(220);
-    rect(30, 20, 55, 55);
-  }
-  
