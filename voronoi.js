76e3f815bd1184ead19af78a9389edba467cd265
gauthier909 <78608563+grogerfr@users.noreply.github.com>
2022-11-26 01:41:47 +0100

    smallest circle

--- voronoi.js
+++ voronoi.js
@@ -15,14 +15,23 @@ class Line {
     }
 }
 
+class Circle {
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+}
 
 var convexHull = [];
 var points = [];
 var lines = [];
+var circles = [];
+var showCenter = false;
 
 
 function setup() {
-    createCanvas(700, 400);
+    createCanvas(1000, 700);
 
     button = createButton('Add random point');
 
@@ -36,9 +45,9 @@ function setup() {
 
     button3.mousePressed(smallestCircle);
 
-    button.position(30, 600);
-    button2.position(200, 600);
-    button3.position(300, 600);
+    button.position(1000, 900);
+    button2.position(1150, 900);
+    button3.position(1250, 900);
 }
 
 
@@ -52,23 +61,23 @@ function draw() {
     for (i in lines) {
         line(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
     }
+    for (i in circles) {
+        circle(circles[i].x, circles[i].y, circles[i].r);
+        fill(255, 0, 0, 127);
+    }
 
-
+    if(showCenter){
+        ellipse(circles[0].x,circles[0].y,10,10)
+    }
 
 }
 
 function randomPoint() {
-    let width = Math.floor(Math.random() * 701);
-    let height = Math.floor(Math.random() * 401);
+    let width = Math.floor(Math.random() * 501);
+    let height = Math.floor(Math.random() * 501);
     let p = new Point(width, height);
     points.push(p);
 }
-
-
-windowResized = function () {
-    resizeCanvas(windowWidth, windowHeight);
-};
-
 
 
 
@@ -79,11 +88,12 @@ function drawCH() {
                 if (isSegmentConvex(points[i], points[j], points)) {
                     let l = new Line(points[i].x, points[i].y, points[j].x, points[j].y);
                     lines.push(l);
-                    addInConvexHull(points[i],points[j]);
+                    addInConvexHull(points[i], points[j]);
                 }
             }
         }
     }
+    console.log(convexHull);
 }
 
 function isSegmentConvex(point1, point2, points) {
@@ -102,10 +112,67 @@ function isSegmentConvex(point1, point2, points) {
     return true;
 }
 
-function addInConvexHull(point1,point2){
+function addInConvexHull(point1, point2) {
+    if (!convexHull.some((e) => (e.x === point1.x && e.y === point1.y))) {
+        convexHull.push(point1);
+    }
+    if (!convexHull.some((e) => (e.x === point2.x && e.y === point2.y))) {
+        convexHull.push(point2);
+    }
 
 }
 
-function smallestCircle(){
-
+function smallestCircle() {
+    const result = (weltz(convexHull, convexHull.length, [], 0));
+    let smallestCircle = new Circle(result.x, result.y, result.r * 2);
+    circles.push(smallestCircle);
+    showCenter = true;
 }
+
+function weltz(convexHull, nbrPoints, bound, b) {
+    let circle;
+    if (b === 3) circle = check3(bound[0], bound[1], bound[2])
+    else if (nbrPoints === 1 && b === 0) circle = { x: convexHull[0].x, y: convexHull[0].y, r: 0 }
+    else if (nbrPoints === 0 && b === 2) circle = check2(bound[0], bound[1])
+    else if (nbrPoints === 1 && b === 1) circle = check2(bound[0], convexHull[0])
+    else {
+        circle = weltz(convexHull, nbrPoints - 1, bound, b)
+        if (!inCircle(convexHull[nbrPoints - 1], circle)) {
+            bound[b++] = convexHull[nbrPoints - 1]
+            circle = weltz(convexHull, nbrPoints - 1, bound, b)
+        }
+    }
+    return circle;
+}
+
+
+function check3(p1, p2, p3) {
+    let
+        a = p2.x - p1.x,
+        b = p2.y - p1.y,
+        c = p3.x - p1.x,
+        d = p3.y - p1.y,
+        e = a * (p2.x + p1.x) * 0.5 + b * (p2.y + p1.y) * 0.5,
+        f = c * (p3.x + p1.x) * 0.5 + d * (p3.y + p1.y) * 0.5,
+        det = a * d - b * c,
+        cx = (d * e - b * f) / det,
+        cy = (-c * e + a * f) / det
+
+    return { x: cx, y: cy, r: Math.sqrt((p1.x - cx) * (p1.x - cx) + (p1.y - cy) * (p1.y - cy)) }
+}
+
+function check2(p1, p2) {
+    let
+        cx = 0.5 * (p1.x + p2.x),
+        cy = 0.5 * (p1.y + p2.y)
+
+    return { x: cx, y: cy, r: Math.sqrt((p1.x - cx) * (p1.x - cx) + (p1.y - cy) * (p1.y - cy)) }
+}
+
+function inCircle(p, c) {
+    return ((c.x - p.x) * (c.x - p.x) + (c.y - p.y) * (c.y - p.y) <= c.r * c.r)
+}
+
+windowResized = function () {
+    resizeCanvas(windowWidth, windowHeight);
+};
